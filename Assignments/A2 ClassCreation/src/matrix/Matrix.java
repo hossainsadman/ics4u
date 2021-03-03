8b1f034fcfff399af3a9bc4856c8a63ea7835936
Sadman Hossain <m.sadman.h@gmail.com>
2021-03-03 15:13:12 -0500

    Set up classes for A2 (class creation)

---
+++ Assignments/A2 ClassCreation/src/matrix/Matrix.java
@@ -0,0 +1,16 @@
+package matrix;
+
+/**
+ *
+ * @author Sadman
+ */
+public class Matrix {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+	// TODO code application logic here
+    }
+
+}
---
+++ Assignments/A2 ClassCreation/src/matrix/Test.java
@@ -0,0 +1,16 @@
+package matrix;
+
+/**
+ *
+ * @author Sadman
+ */
+public class Test {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
