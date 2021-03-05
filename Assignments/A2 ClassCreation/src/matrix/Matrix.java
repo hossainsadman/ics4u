27c056684c4cd8dd5ec187aff43948610bfdbf27
Sadman Hossain <m.sadman.h@gmail.com>
2021-03-04 20:13:46 -0500

    Add some javadocs and comments

--- Assignments/A2 ClassCreation/src/matrix/Matrix.java
+++ Assignments/A2 ClassCreation/src/matrix/Matrix.java
@@ -6,12 +6,15 @@ package matrix;
  */
 public class Matrix {
 
+    // number of rows and columns in the matrix
     private int rows;
     private int cols;
+
+    // 2D array to contain the elements of the matrix
     private double[][] array;
 
     /**
-     * Initialize an empty matrix with null elements
+     * Initialize an empty matrix filled with null elements
      * @param row number of rows
      * @param col number of columns
      */
@@ -21,32 +24,51 @@ public class Matrix {
         array = new double[rows][cols];
     }
 
+    /**
+     * Initilize a matrix using a 2D array of doubles
+     * @param array 2D array of doubles
+     */
     public Matrix(double[][] array) {
         this.rows = array.length;
         this.cols = array[0].length;
         this.array = array;
     }
 
+    /**
+     * Initialize a matrix using a 1D array of doubles
+     * @param row 1D array of doubles
+     */
     public Matrix(double[] row) {
-        this.rows = row.length;
-        this.cols = 1;
-        array = new double[rows][1];
+        // since a 1D array is used, the matrix only has 1 row
+        this.rows = 1;
+        this.cols = row.length;
+        array = new double[this.rows][this.cols];
         array[0] = row;
     }
 
+    /**
+     * Initialize a matrix as a copy of another matrix
+     * @param m matrix to be copied
+     */
     public Matrix(Matrix m) {
         this.rows = m.rows;
         this.cols = m.cols;
         this.array = m.array;
     }
 
+    /**
+     * Return a string representation of the matrix object (with box brackets).
+     */
     @Override
 	public String toString() {
+        // format top of matrix with spaces between '┌' and '┐'
         String str = "┌ ";
         for (int i = 0; i < cols; i++) {
             str += "  ";
         }
         str += "┐\n";
+
+        // format matrix elements with rows beginning and ending with '│'
         for (int row = 0; row < rows; row++) {
             str += "│ ";
             for (int col = 0; col < cols; col++) {
@@ -58,6 +80,8 @@ public class Matrix {
             }
             str += "│\n";
         }
+
+        // format bottom of matrix with spaces between '└' and '┘'
         str += "└ ";
         for (int i = 0; i < cols; i++) {
             str += "  ";
@@ -66,28 +90,61 @@ public class Matrix {
         return str;
     }
 
+    /**
+     * Return the number of rows in the matrix
+     * @return number of rows in the matrix
+     */
     public int getNumOfRows() {
         return this.rows;
     }
 
+    /**
+     * Return the number of columns in the matrix
+     * @return number of columns in the matrix
+     */
     public int getNumOfCols() {
         return this.cols;
     }
 
+    /**
+     * Set the elements of a row in the matrix with elements from
+     * another 1D array
+     * @param row index of the row to be set
+     * @param rowArray 1D array containing the new elements of the row
+     */
     void setRow(int row, double[] rowArray) {
         this.array[row] = rowArray;
     }
 
+    /**
+     * Set the elements of a column in the matrix with elements from
+     * another 1D array
+     * @param col index of the column to be set
+     * @param colArray 1D array containing the new elements of the column
+     */
     void setCol(int col, double[] colArray) {
         for (int row = 0; row < rows; row++) {
             array[row][col] = colArray[row];
         }
     }
 
+    /**
+     * Set the element in a specific row and column in the matrix
+     * @param row index of the row of the element
+     * @param col index of the column of the element
+     * @param element value of the element to be set
+     */
     void setElement(int row, int col, double element) {
         array[row][col] = element;
     }
 
+    /**
+     * Returns whether the dimensions (number of rows and columns) of this
+     * matrix is equal to the dimensions of another matrix
+     * @param m another matrix that this matrix is being compared to
+     * @return whether the dimensions of this matrix is equal to the
+     * dimensions of m
+     */
     public boolean equalDimensions(Matrix m) {
         if (rows == m.rows && cols == m.cols) {
             return true;
@@ -95,10 +152,22 @@ public class Matrix {
         return false;
     }
 
+    /**
+     * Returns whether the dimensions (number of rows and columns) and values
+     * of elements of this matrix are equal to dimensions and elements of
+     * another matrix
+     * @param m another matrix that this matrix is being compared to
+     * @return whether the dimensions and elements of this matrix are equal to that of m
+     */
     public boolean equals(Matrix m) {
+        // if the matrices do not have the same dimensions, they cannot be
+        // equal
         if (!equalDimensions(m)) {
             return false;
         }
+
+        // if the elements of the matrices differ at any row or column, they
+        // cannot be equal
         for (int row = 0; row < rows; row++) {
             for (int col = 0; col < cols; col++) {
                 if (array[row][col] != m.array[row][col]) {
@@ -106,9 +175,16 @@ public class Matrix {
                 }
             }
         }
+
+        // if no differences have been found, the matrices are equal
         return true;
     }
 
+    /**
+     * Return a matrix with a constant added to each element in this matrix
+     * @param d constant to be added to each element
+     * @return
+     */
     public Matrix add(double d) {
         Matrix m = new Matrix(this);
         for (int row = 0; row < rows; row++) {
@@ -119,6 +195,12 @@ public class Matrix {
         return m;
     }
 
+    /**
+     * Return the sum of this matrix and another matrix, where each element is
+     * the sum of the elements at the same row and column in both matrices
+     * @param m another matrix to be summed with this matrix
+     * @return
+     */
     public Matrix sum(Matrix m) {
         Matrix sum = new Matrix(this);
         for (int row = 0; row < rows; row++) {
@@ -129,6 +211,11 @@ public class Matrix {
         return sum;
     }
 
+    /**
+     * Return the sum of elements in a row
+     * @param row index of the row
+     * @return
+     */
     double sumRow(int row) {
         double sum = 0;
         for (int col = 0; col < cols; col++) {
@@ -137,6 +224,11 @@ public class Matrix {
         return sum;
     }
 
+    /**
+     * Return the sum of elements in a column
+     * @param col index of the column
+     * @return
+     */
     double sumCol(int col) {
         double sum = 0;
         for (int row = 0; row < rows; row++) {
@@ -145,6 +237,11 @@ public class Matrix {
         return sum;
     }
 
+    /**
+     * Return the product of this matrix with another matrix, whether the number of rows is equal to the number of rows of this matrix and the number of columns is equal to the number of rows of the other matrix
+     * @param m another matrix to be multiplied with this matrix
+     * @return
+     */
     public Matrix product(Matrix m) {
         Matrix product = new Matrix(rows, m.cols);
         for (int row = 0; row < product.rows; row++) {
